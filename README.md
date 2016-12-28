b31d48d78038918bf14e3e7065047e184bb5c91a
sunyl100 <40603993@qq.com>
2016-12-28 09:47:00 +0800

    1.0
    最基础的

---
+++ README.md
@@ -0,0 +1,20 @@
+# [JFinal][jfinal]
+
+## 项目工具链
+
++ jdk
++ [Gradle][gradle]
+
+## 数据库
+
++ MySQL
+
+### 配置
+
+1. 修改 src/main/resources/a_little_config.txt
+2. 将 blog.sql 导入 MySQL
+
+[jfinal]: http://www.jfinal.com/
+[gradle]: https://gradle.org/
+
+
---
+++ src/main/java/com/demo/test/test1.java
@@ -0,0 +1,15 @@
+package com.demo.test;
+
+import com.sun.corba.se.impl.orbutil.concurrent.Sync;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/12/27 0027.
+ */
+public class test1 {
+    public static void main(String[] args) {
+    System.out.print("");
+    }
+}
